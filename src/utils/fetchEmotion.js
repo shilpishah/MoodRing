2de0b7de593562a214e86ff635141716d25c5ab8
Shilpi Shah <shilpishah2003@gmail.com>
2025-05-26 16:42:14 -0400

    Update fetchEmotion.js

--- src/utils/fetchEmotion.js
+++ src/utils/fetchEmotion.js
@@ -1,5 +1,5 @@
 export default async function fetchEmotion() {
-  const response = await fetch('http://localhost:8000/api/emotion'); // changes according to local host setup
+  const response = await fetch('https://moodring-ochl.onrender.com/api/emotion'); // deployed on render!
   if (!response.ok) throw new Error('Failed to fetch emotion');
   return await response.json();
 }
